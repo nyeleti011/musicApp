b3aebc1932d9f701daae1733e5303b7bff9e7bea
nyeleti011 <nyeletiobey@gmail.com>
2025-07-24 15:44:30 +0200

    classSong.java

---
+++ Song.java
@@ -0,0 +1,18 @@
+package com.moodifyx;
+
+public class Song{
+    private final   String title;
+    private final String filePath;
+
+    public Song(String title, String filePath){
+        this.title= title;
+        this.filePath = filePath;
+
+    }
+Public String
+
+
+
+
+}
+}
